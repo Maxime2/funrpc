785f99d0f29696efe258dbe0b67e35e3991000da
Maxim Zakharov <dp.maxime@gmail.com>
2019-01-15 12:08:12 +1100

    add POST requests for transaction submissions; reformatting with cargo make
    Signed-off-by: Maxim Zakharov <dp.maxime@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
+use actix_web::{actix, client, HttpMessage};
 use structopt::StructOpt;
-use actix_web::{actix,client, HttpMessage};
 #[macro_use]
 extern crate serde_derive;
-use serde_json;
 use futures::future::Future;
-
+use serde_json;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "funrpc")]
@@ -29,12 +28,11 @@ struct Opt {
 
     /// Command
     #[structopt(subcommand)]
-    cmd: Command
-
+    cmd: Command,
 }
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "cmd", help="subcommands")]
+#[structopt(name = "cmd", help = "subcommands")]
 enum Command {
     #[structopt(name = "accounts")]
     /// get controlled accounts
@@ -44,7 +42,7 @@ enum Command {
     /// get any account
     Account {
         #[structopt(short = "n", long = "number")]
-        number: String
+        number: String,
     },
     #[structopt(name = "tx")]
     /// sends transaction or get transaction status
@@ -54,10 +52,9 @@ enum Command {
     /// sends raw signed transaction
     RawTx {
         #[structopt(short = "d", help = "raw transaction")]
-        doc: String
-    }
+        doc: String,
+    },
 }
-
 
 #[derive(StructOpt, Debug, Serialize)]
 struct TxD {
@@ -69,7 +66,6 @@ struct TxD {
     value: String,
 }
 
-
 #[derive(StructOpt, Debug)]
 enum Tx {
     #[structopt(name = "d")]
@@ -80,17 +76,15 @@ enum Tx {
     #[structopt(name = "h")]
     H {
         /// Hexadecimal transaction hash
-        tx_hash: String
-    }
+        tx_hash: String,
+    },
 }
-
-
-
 
 fn main() {
     let opt = Opt::from_args();
     let mut url = String::new();
     let mut body = "".to_string();
+    let mut post: bool = false;
     //println!("{:?}", opt);
     if opt.verbose > 0 {
         if opt.debug {
@@ -100,60 +94,67 @@ fn main() {
     }
     match opt.cmd {
         Command::Accounts => {
-            url = format!("http://{}:{}/accounts", opt.hostname, opt.port); 
+            url = format!("http://{}:{}/accounts", opt.hostname, opt.port);
             if opt.verbose > 0 {
                 println!("Controlled accounts from {}:{}", opt.hostname, opt.port);
             }
-
-        },
+        }
         Command::Account { number } => {
             url = format!("http://{}:{}/account/{}", opt.hostname, opt.port, number);
             if opt.verbose > 0 {
                 println!("Account from {}:{}", opt.hostname, opt.port);
             }
-        },
-        Command::Tx( tx ) => {
+        }
+        Command::Tx(tx) => {
             url = format!("http://{}:{}/tx", opt.hostname, opt.port);
             match tx {
                 Tx::H { tx_hash } => {
                     url = format!("{}/{}", url, tx_hash);
                 }
-                Tx::D ( tx_d ) => {
+                Tx::D(tx_d) => {
+                    post = true;
                     body = serde_json::to_string(&tx_d).unwrap();
                 }
             }
-        },
+        }
         Command::RawTx { doc } => {
+            post = true;
             body = doc;
             url = format!("http://{}:{}/rawtx", opt.hostname, opt.port);
             if opt.verbose > 0 {
-                println!("Sending raw signed transaction to {}:{}", opt.hostname, opt.port);
+                println!(
+                    "Sending raw signed transaction to {}:{}",
+                    opt.hostname, opt.port
+                );
             }
         }
     }
 
-actix::run(
-        ||
-
-    client::ClientRequest::get(url) // <- Create request builder
-            .header("User-Agent", "Actix-web")
-            .body(actix_web::Body::from(body)).unwrap()
-            .send()
-            .map_err(|_| ())
-            .and_then(|response| {                     // <- server http response
-                match response.body().wait() {
-                    Ok(body) => {
-                        let _foo: String = String::from_utf8(body.to_vec()).unwrap();
-                        println!("{}", _foo);
-                        actix::System::current().stop();
-                        Ok(())
-                    },
-                    Err(_err) => {
-                        println!("error {:?}", _err);
-                        actix::System::current().stop();
-                        Err(())
-                    }
+    actix::run(|| {
+        match post {
+            false => client::ClientRequest::get(url),
+            true => client::ClientRequest::post(url),
+        }
+        .header("User-Agent", "Actix-web")
+        .body(actix_web::Body::from(body))
+        .unwrap()
+        .send()
+        .map_err(|_| ())
+        .and_then(|response| {
+            // <- server http response
+            match response.body().wait() {
+                Ok(body) => {
+                    let _foo: String = String::from_utf8(body.to_vec()).unwrap();
+                    println!("{}", _foo);
+                    actix::System::current().stop();
+                    Ok(())
                 }
-            })
-    );
+                Err(_err) => {
+                    println!("error {:?}", _err);
+                    actix::System::current().stop();
+                    Err(())
+                }
+            }
+        })
+    });
 }
